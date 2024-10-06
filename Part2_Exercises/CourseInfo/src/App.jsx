d47685dc649171345be613856d299ed3007f2687
Mujahid Iqbal <sayedmujahidiqbal@gmail.com>
2024-10-06 15:02:41 +0300

    Course update Exercise 2.4 commit

--- Part2_Exercises/CourseInfo/src/App.jsx
+++ Part2_Exercises/CourseInfo/src/App.jsx
@@ -1,39 +1,41 @@
-const Header = (props) => {
-  return(
-    <h1>
-      {props.course.name}
-    </h1>
-  )
-}
-
-const Content = (props) =>{
-  return(
-    props.course.parts.map(part => <Part key={part.name} part={part.name} exercises={part.exercises} />)
-  )
-}
-
-const Total = (props) => {
-  let totalExercises= 0
-  props.course.parts.map(part => totalExercises +=  part.exercises)
-
-  return <p><b>Total of {totalExercises} Exercise</b></p>
-}
-
-const Part = (props) => { 
-  return(
-    <p>
-      {props.part} {props.exercises}
-    </p>
-  )
-}
-
-const Course = ({course}) => {
+const Course = ({ course }) => {
   return(
     <div>
         <Header course={course} />
         <Content course={course} />
         <Total course={course} />
     </div>
+  )
+}
+
+const Header = ({ course }) => {
+  return(
+    <h1>
+      {course.name}
+    </h1>
+  )
+}
+
+const Content = ({ course }) =>{
+  return(
+    course.parts.map(part => <Part key={part.name} part={part.name} exercises={part.exercises} />)
+  )
+}
+
+const Total = ({ course }) => {
+  const total = course.parts.reduce((prevExercises, currExercises) => {   
+    return prevExercises + currExercises.exercises
+  }, 0)
+
+  return <p><b>Total of {total} Exercise</b></p>
+}
+
+const Part = (props) => {
+  const { part, exercises } = props 
+  return(
+    <p>
+      {part} {exercises}
+    </p>
   )
 }
 
