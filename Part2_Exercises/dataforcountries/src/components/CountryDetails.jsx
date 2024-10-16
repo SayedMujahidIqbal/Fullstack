89ce7cfcc807d84d9069d0a41f8f876b6b8a3eea
Mujahid Iqbal <sayedmujahidiqbal@gmail.com>
2024-10-16 10:48:25 +0300

    Part 2 final commit

--- Part2_Exercises/dataforcountries/src/components/CountryDetails.jsx
+++ Part2_Exercises/dataforcountries/src/components/CountryDetails.jsx
@@ -3,22 +3,21 @@ import CountryFlag from './CountryFlag'
 import CountryLanguages from './CountryLanguages'
 import axios from 'axios'
 
-const api_key = import.meta.env.VITE_SOME_KEY
+const api_key = import.meta.env.VITE_API_KEY
 
 const CountryDetails = ({ country }) => {
     const [weatherDetails, setWeahterDetails] = useState(null)
     const capital = country.capital
+    const iconCode = weatherDetails?.weather?.map(weather => weather.icon)
 
-    useEffect(() =>{
+    useEffect(() => {
         axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${capital}&appid=${api_key}`)
         .then(response => {
             setWeahterDetails(response.data)
         }).catch(error => {
             console.log(error)
         })
-    }, [])
-
-    console.log(weatherDetails)
+    }, [capital])
 
   return (
     <>
@@ -28,9 +27,17 @@ const CountryDetails = ({ country }) => {
         <h2>languages</h2>
         <CountryLanguages country={country} />
         <CountryFlag country={country} />
-        <h2>Weather in {country.capital}</h2>
-        {/* <image src={weatherDetails[0]} /> */}
-        <p><b>Wind {weatherDetails.wind['speed']} m/s</b></p>
+        {
+            weatherDetails &&  
+            <>
+                <h2>Weather in {country.capital}</h2>
+                <p><b>Temperature { (Math.round(weatherDetails?.main['temp'] - 273.15) * 100 / 100).toFixed(2) } Celcius</b></p>
+                <div>
+                    <img src={`https://openweathermap.org/img/wn/${iconCode}@4x.png`} alt={country.name.common} />
+                </div>
+                <p><b>Wind {weatherDetails?.wind['speed']} m/s</b></p>
+            </>
+        }
     </>
   )
 }
