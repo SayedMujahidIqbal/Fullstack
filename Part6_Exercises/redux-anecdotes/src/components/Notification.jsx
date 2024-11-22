5cbced33188e3a3230854517c72614a2dfc97194
Mujahid Iqbal <sayedmujahidiqbal@gmail.com>
2024-11-22 15:29:54 +0300

    part 6 exercise 6.12 Better Anecdotes, step 10

--- Part6_Exercises/redux-anecdotes/src/components/Notification.jsx
+++ Part6_Exercises/redux-anecdotes/src/components/Notification.jsx
@@ -4,8 +4,10 @@ const Notification = () => {
   const notification = useSelector(state => state.notification)
   const style = {
     border: '10px solid green',
+    borderRadius: '10px',
     padding: 10,
-    borderWidth: 1
+    borderWidth: 5,
+    marginBottm: 10
   }
   return (    
     <div style={style}>
