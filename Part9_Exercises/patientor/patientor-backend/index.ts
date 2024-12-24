78798112cca8ccfca0b832106b37bf4a86f17ee7
Mujahid Iqbal <sayedmujahidiqbal@gmail.com>
2024-12-24 14:27:22 +0300

    part 9 exercises 9.8 and 9.9 commit

---
+++ Part9_Exercises/patientor/patientor-backend/index.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static("dist"));
+
+app.get("/api/ping", (_req, res) => {
+  res.send("pong");
+  console.log("someone pinged me");
+});
+
+const PORT = 3003;
+
+app.listen(PORT, () => {
+  console.log(`Server is listening at ${PORT}`);
+});
